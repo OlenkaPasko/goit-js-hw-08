28e0854e1b8eabe57ae691a0eee5430f45434ecb
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-03-14 22:49:35 +0200

    video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,17 +5,21 @@ const iframe = document.querySelector('iframe');
 
 const player = new Player(iframe);
 
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time');
+};
 
-player.on('timeupdate', function (data) {
-  
-});
-/*const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+player.on('timeupdate', throttle(onPlay, 1000));
 
-player.on('play', function () {
-  console.log('played the video!');
-});
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});*/
+const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
