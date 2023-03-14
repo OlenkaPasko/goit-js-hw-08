4198c0a88008620b7060178a6cd070e8322761d8
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-03-14 23:49:23 +0200

    ключ зберігання

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+
+
+/*const form = document.querySelector("#message-form");
+const output = document.querySelector("#output");
+const LOCALSTORAGE_KEY = "goit-example-message";
+
+updateOutput();
+form.addEventListener("submit", saveMessage);
+
+function saveMessage(evt) {
+  evt.preventDefault();
+  localStorage.setItem(LOCALSTORAGE_KEY, form.elements.message.value);
+  updateOutput();
+  form.reset();
+}
+
+function updateOutput() {
+  output.textContent = localStorage.getItem(LOCALSTORAGE_KEY) || "";
+}*/
