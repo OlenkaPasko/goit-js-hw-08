22b72884a02391b8baffaae77cc72eeb4eab1174
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-03-16 14:42:51 +0200

    onFormSubmit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,26 +11,39 @@ const refs = {
 refs.input.addEventListener('input', throttle(onTextareaInput, 500))
 refs.form.addEventListener('submit', onFormSubmit);
 
-
-//Відстежуй на формі подію input і 
-//записуй у локальне сховище об'єкт з полями email і message;
-//ключем для сховища буде рядок "feedback-form-state"
-
 function onTextareaInput(event) {
   formData[event.target.name] = event.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.parse(formData));
 }
 
-
 //зберігай поточні значення полів форми
 //завантаження сторінки перевіряй стан сховища;
 //якщо там є збережені дані, заповнюй ними поля форми.
 //В іншому випадку поля повинні бути порожніми.
-function onFormSubmit(event) {}
+/*Сабміт форми перезавантажує сторінку, тому не забувайте скасовувати 
+дію за замовчуванням методом preventDefault().*/ 
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    if (refs.input.value === '' || refs.textarea.value === '') {
+      return alert(`Please fill in all the fields!`);
+    }
+    or;
+    const { email, message } = event.currentTarget.elements;
+    console.log({ email: email.value, message: message.value });
+    or;
+    console.log(formData);
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    formData = {};
+  }
+
 //Під час сабміту форми очищуй сховище і поля форми,
 // а також виводь у консоль об'єкт з полями 
 //email, message та їхніми поточними значеннями.
-function messageForm(event) {}
+function messageForm(event) {
+}
+
 /*const form = document.querySelector("#message-form");
 const output = document.querySelector("#output");
 const LOCALSTORAGE_KEY = "goit-example-message";
