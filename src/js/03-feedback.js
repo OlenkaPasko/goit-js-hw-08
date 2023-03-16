69a58c96cb6924f2c1fff060aec7b71ece3cf2e2
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-03-16 14:15:12 +0200

    function

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,33 @@
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.feedback-form');
-const input = document.querySelector('input');
-const textarea = document.querySelector('textarea');
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+}
+//const STORAGE_KEY = 'feedback-form-state';
+refs.form.addEventListener('submit', messageForm);
+refs.input.addEventListener('input', )
 
-const STORAGE_KEY = 'feedback-form-state';
+//записуй у локальне сховище об'єкт з полями email і message;
+//зберігай поточні значення полів форми
+//ключем для сховища буде рядок "feedback-form-state"
+function messageForm(event) {
 
+}
 
+//завантаження сторінки перевіряй стан сховища;
+//якщо там є збережені дані, заповнюй ними поля форми.
+//В іншому випадку поля повинні бути порожніми.
+function (event) {
+  
+}
+//Під час сабміту форми очищуй сховище і поля форми,
+// а також виводь у консоль об'єкт з полями 
+//email, message та їхніми поточними значеннями.
+function () {
 
+}
 /*const form = document.querySelector("#message-form");
 const output = document.querySelector("#output");
 const LOCALSTORAGE_KEY = "goit-example-message";
