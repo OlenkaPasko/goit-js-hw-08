dcf4332b43c5d1a7115cc4cc3e97dc246af22ba6
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-03-17 07:41:58 +0200

    feedback-form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,28 +1,18 @@
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
+
+form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(onTextareaInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
 let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 const { email, message } = form.elements;
-messageForm();
-
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  input: document.querySelector('.feedback-form input'),
-  textarea: document.querySelector('.feedback-form textarea'),
-};
-refs.input.addEventListener('input', throttle(onTextareaInput, 500));
-refs.form.addEventListener('submit', onFormSubmit);
-
-/*Відстежуй на формі подію input, і щоразу записуй у 
-окальне сховище об'єкт з полями email і message, 
-у яких зберігай поточні значення полів форми. 
-Нехай ключем для сховища буде рядок "feedback-form-state".*/
 
 function onTextareaInput(event) {
-  formData = { email: email.value, message: message.value }; 
+  formData = { email: email.value, message: message.value };
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
-
 
 function onFormSubmit(event) {
   event.preventDefault();
