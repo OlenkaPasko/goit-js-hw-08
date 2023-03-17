f87f3ac988b1ef6cbc577a819a2e24c576e64f77
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-03-17 23:43:39 +0200

    const form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,7 +2,7 @@ import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
 
-form = document.querySelector('.feedback-form');
+const form = document.querySelector('.feedback-form');
 
 form.addEventListener('input', throttle(onTextareaInput, 500));
 form.addEventListener('submit', onFormSubmit);
